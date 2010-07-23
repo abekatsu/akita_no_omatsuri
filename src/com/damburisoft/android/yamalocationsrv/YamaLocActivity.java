210407ce9e2c1654b5243d4e8204d632b8faf8d1
Katsuhisa ABE <katsuhisa.abe@nifty.com>
2010-07-23 20:43:01 +0900

    Initial project for YamaLocationService

---
+++ src/com/damburisoft/android/yamalocationsrv/YamaLocActivity.java
@@ -0,0 +1,13 @@
+package com.damburisoft.android.yamalocationsrv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YamaLocActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
